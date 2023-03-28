177fdda1b8b85b2ccdc001c3cceebcf41ffde325
Chloe <chloesman1999@gmail.com>
2023-03-28 15:11:48 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lootbox-study/',
+  base: '/',
   plugins: [react()],
   test: {
     globals: true,
